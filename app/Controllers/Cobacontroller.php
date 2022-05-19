a0e8db91a4f799862142b305d3e3091840895886
dwinurselviana <selvianadwinur@gmail.com>
2022-05-19 09:19:42 +0700

    selvia

---
+++ app/Controllers/Cobacontroller.php
@@ -0,0 +1,12 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Cobacontroller extends Controller{
+    public function index()
+    {
+        return view('coba2');
+    }
+
+}
